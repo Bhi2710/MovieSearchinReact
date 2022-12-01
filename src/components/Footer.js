2dfdba926bdb14d58b0841e32134fe51517556d0
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2022-12-01 11:03:57 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = _ => {
     <Box pb={4}>
       <Typography variant='h6' align='center'>Developed by Aayush &amp; Team</Typography>
       <Typography variant='h6' align='center'>
-        <a href="https://github.com/Bhi2710/cinematic" target="_blank" rel="noreferrer">
+        <a href="https://github.com/Bhi2710/MovieSearchinReact" target="_blank" rel="noreferrer">
           Github Source Code
         </a>
       </Typography>
