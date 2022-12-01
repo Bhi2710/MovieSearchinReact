df80272954a4975aa8527cf7d1389585b2a54f5d
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2022-12-01 11:14:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import {Box, Typography} from '@mui/material';
 const Footer = _ => {
   return (
     <Box pb={4}>
-      <Typography variant='h6' align='center'>Developed by Aayush &amp; Team</Typography>
+      <Typography variant='h6' align='center'>Developed by Abhishek Yadav</Typography>
       <Typography variant='h6' align='center'>
         <a href="https://github.com/Bhi2710/MovieSearchinReact" target="_blank" rel="noreferrer">
           Github Source Code
